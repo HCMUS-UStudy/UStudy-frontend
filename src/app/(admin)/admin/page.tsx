02888e62e281529223fed0a9664073f15335fac5
voanhquan13072003@gmail.com <vaquan21@clc.fitus.edu.vn>
2024-11-06 14:15:48 +0700

    change admin page

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -2,14 +2,14 @@ import Header from "@/app/ui/components/header";
 import PageWrapper from "@/app/ui/components/pagewrapper";
 import { SideBar } from "@/app/ui/sidebar/sidebar";
 
-export default function Admin({ children }: { children: React.ReactNode }) {
-    return (
-        <>
-            <SideBar />
-            <div className="flex flex-col h-full w-full">
-                <Header />
-                <PageWrapper children={children} />
-            </div>
-        </>
-    )
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <SideBar />
+      <div className="flex flex-col h-full w-full">
+        <Header />
+        <PageWrapper children={children} />
+      </div>
+    </>
+  );
 }
