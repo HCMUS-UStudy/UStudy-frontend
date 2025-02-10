e82bc118f23d11bcb5820f94544771f298250763
mquaan <nmquan.se@gmail.com>
2025-02-10 15:10:46 +0700

    edit color and admin login

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,15 +10,17 @@ const config: Config = {
     extend: {
       colors: {
         // primary: "#2563eb", // blue-600
-        primary: "BEE5D1",
+        primary: "#BEE5D1",
+        "primary-light": "#d5e9e1",
         // "primary-dark": "#1e40af", // blue-800
-        "primary-dark": "#95BFA9", 
+        "primary-dark": "#95BFA9",
 
         neutral: "#d1d5db", // gray-300
 
         background: "#f1f5f9", // slate-50: #f8fafc, #f9fbfc, slate-100: #f1f5f9
+        // background: "#F6F5F5",
         hero: "#dbeafe", // #D5E9F6, blue-100: #dbeafe
-        foreground: "var(--foreground)",
+        foreground: "#feffff", // white
 
         "disabled-dark": "#9ca3af", // gray-400
         "disabled-light": "#d1d5db", // gray-300
@@ -42,31 +44,31 @@ const config: Config = {
       },
       keyframes: {
         scale: {
-          '0%': { transform: 'scale(1)'},
-          '50%': {transform: 'scale(1.3)'},
-          '100%': {transform: 'scale(1)'}
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.3)" },
+          "100%": { transform: "scale(1)" },
         },
         fall: {
-          '0%': { transform: 'translateY(0vh)' },
-          '50%': { transform: 'translateY(50vh)' },
-          '100%': { transform: 'translateY(100vh)' }
+          "0%": { transform: "translateY(0vh)" },
+          "50%": { transform: "translateY(50vh)" },
+          "100%": { transform: "translateY(100vh)" },
         },
         wiggle: {
-          '0%': {
-            'transform-origin': 'bottom',
-            transform: 'rotate(0deg)'
+          "0%": {
+            "transform-origin": "bottom",
+            transform: "rotate(0deg)",
           },
-          '25%': {
-            'transform-origin': 'bottom left',
-            transform: 'rotate(-15deg)'
+          "25%": {
+            "transform-origin": "bottom left",
+            transform: "rotate(-15deg)",
           },
-          '50%': {
-            'transform-origin': 'bottom',
-            transform: 'rotate(0deg)'
+          "50%": {
+            "transform-origin": "bottom",
+            transform: "rotate(0deg)",
           },
-          '75%': {
-            'transform-origin': 'bottom right',
-            transform: 'rotate(15deg)'
+          "75%": {
+            "transform-origin": "bottom right",
+            transform: "rotate(15deg)",
           },
           "100%": {
             "transform-origin": "bottom",
@@ -91,11 +93,11 @@ const config: Config = {
         "color-change-1": "color_change 1s linear 0s infinite",
         "color-change-2": "color_change 1s linear 0.2s infinite",
         "color-change-3": "color_change 1s linear 0.4s infinite",
-        fall_1: 'fall 2.5s linear 0s infinite',
-        fall_2: 'fall 3s linear 1s infinite',
-        fall_3: 'fall 2.5s linear 2s infinite',
-        fall_4: 'fall 3s linear 1.5s infinite',
-        fall_5: 'fall 3s linear 2.5s infinite',
+        fall_1: "fall 2.5s linear 0s infinite",
+        fall_2: "fall 3s linear 1s infinite",
+        fall_3: "fall 2.5s linear 2s infinite",
+        fall_4: "fall 3s linear 1.5s infinite",
+        fall_5: "fall 3s linear 2.5s infinite",
       },
     },
   },
