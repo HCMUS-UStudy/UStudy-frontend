a8993a531f0b0b5e447c148d5622c10ef2a80b14
voanhquan13072003@gmail.com <vaquan21@clc.fitus.edu.vn>
2025-02-13 10:38:58 +0700

    format login code, add auth context, change login api

---
+++ src/app/context/AuthContext.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+  role: string;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
+  return context;
+}
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [role, setRole] = useState<string>("");
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  if (typeof window !== "undefined") {
+    // const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
+    //   const token = localStorage.getItem("accessToken");
+    //   setRole("");
+    //   return token !== null;1
+    // });
+    const accessToken = localStorage.getItem("accessToken");
+    const role = localStorage.getItem("role");
+    setIsAuthenticated(accessToken !== null ? true : false);
+    setRole(role === null ? "" : role);
+  }
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      console.log("server");
+    } else {
+      console.log(localStorage.getItem("accessToken"));
+      console.log("client");
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, role, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/app/ui/components/_common/Button.tsx
+++ src/app/ui/components/_common/Button.tsx
@@ -68,7 +68,7 @@ const Button: React.FC<ButtonProps> = ({
           "tracking-widest hover:shadow-lg border-[1.5px] border-button-primary bg-transparent hover:bg-button-primary/10 text-button-primary hover:shadow-button-primary/20 transition-all duration-200 disabled:border-disabled-dark disabled:text-disabled-dark disabled:hover:bg-transparent disabled:hover:shadow-none":
             variant === "outlined",
         },
-        "flex items-center justify-center w-full font-medium px-3 py-2 rounded-lg disabled:cursor-auto",
+        "flex items-center justify-center font-medium px-3 py-2 rounded-lg disabled:cursor-auto",
         className,
       )}
       {...props}
