8a55649324a2b1d5f9e0aa129aee3d0732fbd5b9
mquaan <nmquan.se@gmail.com>
2025-03-04 14:26:36 +0700

    edit axios

--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -12,7 +12,7 @@ import { redirect } from "next/navigation";
 const requestUrl = ["/auth/login"];
 
 const axiosInstance = axios.create({
-  baseURL: "http://54.252.148.74:8080/api",
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
