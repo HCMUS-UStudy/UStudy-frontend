05e9e017a630a50ce6f266f5ed69cf8835331c40
Nghia <nltnghia21@gmail.com>
2025-03-21 13:58:03 +0700

    Update accessToken and attendance

--- src/app/lib/services/attendance.ts
+++ src/app/lib/services/attendance.ts
@@ -2,19 +2,21 @@ import axiosInstance from "@/app/lib/axios";
 import { AttendaceData } from "@/app/types/type";
 
 export const getAllAttendances = async (
-  classId: string,
   currentPage: number,
   limit: number,
   classScheduleId: string,
   status: string,
 ): Promise<AttendaceData> => {
-  const response = await axiosInstance.get(`/attendance/list/${classId}`, {
-    params: {
-      page: currentPage,
-      limit: limit,
-      classScheduleId: classScheduleId,
-      status: status,
+  const response = await axiosInstance.get(
+    `/attendance/list/${classScheduleId}`,
+    {
+      params: {
+        page: currentPage,
+        limit: limit,
+        classScheduleId: classScheduleId,
+        status: status,
+      },
     },
-  });
+  );
   return response.data.data;
 };
