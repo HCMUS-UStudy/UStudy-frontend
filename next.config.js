1ab11a51e69ec9445f91166f8da7d3c9202d247e
huylpq <huylpq@gmail.com>
2025-07-06 14:49:59 +0700

    test fix url s3

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ustudy.s3.ap-southeast-1.amazonaws.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
