4b8e330a5068aff11374a1502545165faf540aca
huylpq <huylpq@gmail.com>
2025-07-06 15:24:39 +0700

    Rollback to 69dacfb0

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    // domains: ["ustudy.s3.ap-southeast-1.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ustudy.s3.ap-southeast-1.amazonaws.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
